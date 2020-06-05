e83366a44c46ecd7a1929545db829e10269fa960
wtnbtkr <wtnbtkr0000@gmail.com>
2020-06-05 22:27:57 +0900

    finish made

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
 }
 
 html {
   background-color: #212121;
+  padding: 0%;
+  margin: 0%;
+
 }
 </style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
+    <div class="title-board">
         <img class="img" src="../../assets/sea.jpg"/>
+        <div class="text">
+            <img class="logo" src="../../assets/nighthawk.png">
+            <p class="name">tkr wtnb</p>
+            <p>i'm mobile app developer</p>
+        </div>
+
     </div>
 </template>
 
@@ -15,6 +21,34 @@ export default {
 .img {
     width: 100%;
     height: auto;
+    padding: 0px;
+    margin: 0px;
+    opacity: 0.82;
+}
+
+.logo {
+    width: 17vw;
+    border-radius: 17vw;
+}
+
+.title-board {
+    position: relative;
+}
+
+.name {
+    text-decoration: underline;
+}
+.text {
+    position: absolute;
+    color: #fefefe;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%,-50%);
+    -webkit-transform: translate(-50%,-50%);
+    transform: translate(-50%,-50%);
+    margin:0;
+    padding:0;    
+    font-size: 4vw;
 }
 
 </style>
