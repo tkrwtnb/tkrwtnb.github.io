80923e58512b714eccb97412be5486934f3c0d6c
wtnbtkr <wtnbtkr0000@gmail.com>
2020-06-06 09:56:52 +0900

    initial  commit

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,28 @@
 <template>
   <div id="app">
-    <Title></Title>
-    <AboutMe></AboutMe>
-    <Works></Works>
-    <Footer></Footer>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Title from './components/Title.vue'
-import AboutMe from './components/AboutMe.vue'
-import Works from './components/Works.vue'
-import Footer from './components/Footer.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Title,
-    AboutMe,
-    Works,
-    Footer
+    HelloWorld
   }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, 'Noto Sans JP', sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 0px;
-}
-
-html {
-  background-color: #212121;
-  padding: 0%;
-  margin: 0%;
-
+  margin-top: 60px;
 }
 </style>
--- src/components/Footer.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <p>logo designed by gensuke</p>
-        <p>created by tkr wtnb</p>
-    </div>
-</template>
